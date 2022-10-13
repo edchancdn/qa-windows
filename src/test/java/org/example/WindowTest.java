9c02ddf48ee7b2981592c3c4e1cf33b55085f537
edchancdn <edchan.cdn@gmail.com>
2022-10-13 19:33:56 -0400

    added test comment

--- src/test/java/org/example/WindowTest.java
+++ src/test/java/org/example/WindowTest.java
@@ -19,6 +19,8 @@ public class WindowTest {
     WebDriver driver;
     WebDriverWait wait;
 
+    // test comment
+
     @BeforeSuite
     public void setUp() {
         WebDriverManager.chromedriver().setup();
